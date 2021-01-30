2b3a887c5c2466ef4d9944081f7c68a6f45f7bce
walae-br <b.walae@outlook.com>
2021-01-30 16:37:16 +0800

    initial commit

---
+++ pages_project/urls.py
@@ -0,0 +1,8 @@
+# pages_project/urls
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('pages.urls')), # new
+]
